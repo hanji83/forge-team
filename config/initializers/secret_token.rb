47414c4df672ac1472e51ea19c0c91dbff3632b4
hanji83 <andrew.ac.chen@gmail.com>
2014-05-21 19:02:11 -0700

    changed secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-TeamSports::Application.config.secret_key_base = 'c72bd84d6316143c8d47c98eb2926f7ea7860ce0674f4f339db445dfa97a8dce21b8013b96528c52e378aa167526f2a33dadf2856ce1c2d5f6701968b4299489'
+TeamSports::Application.config.secret_key_base = 'e2e5b3a54a30c7dd6a8a21944fb2e2448949a7b7b320fc4cec20a96de8c51231c098d70cfcb53996ae27c4d4862d12fe82bb41ebbdea912e5dfe88b9da29012f'
